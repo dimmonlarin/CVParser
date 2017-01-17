3fa4934914c2172279637528ab8e21326d60c530
dmitriy <dimmon.larin@gmail.com>
2017-01-17 11:43:26 +0800

    Adding datasource - a json CV file

--- CVParser/CVParser/CVParser/views.py
+++ CVParser/CVParser/CVParser/views.py
@@ -5,14 +5,19 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from CVParser import app
+import os
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(PROJECT_ROOT, "data.json"), "r") as myfile:
+        data=myfile.read()
     return render_template(
         'index.html',
         title='Home Page',
         year=datetime.now().year,
+        cv_data=data
     )
 
